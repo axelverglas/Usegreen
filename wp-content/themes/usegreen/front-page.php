2f69f413dd76089357a7544286d08339db59bc6c
Axel Verglas <axelverglas@outlook.com>
2022-12-03 20:51:46 +0100

    Ajout searchbar et divers modification

--- wp-content/themes/usegreen/front-page.php
+++ wp-content/themes/usegreen/front-page.php
@@ -5,7 +5,7 @@
                 <div class="home__content grid">
                     <div class="home__description">
                         <h1 class="home__title"><?php bloginfo('description') ?></h1>
-                        <p>La sobriété du numérique c'est bien trop important pour être négligé.</p>
+                        <p><?php the_content(); ?></p>
                         <a href="" class="home__button">
                             <span>
                                 <svg width="20" height="8" viewBox="0 0 20 8" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -14,48 +14,76 @@
                             </span>
                         S'inscrire</a>
                     </div>
-    
-                    <div class="home__img">
-                        <img src="/assets/img/illustration_hero.svg" alt="home">
-                    </div>
+
+                    <?php
+		//* Variables ACF pour l'image
+		// L'image ACF est enregistrée en tant que "ID de l'image"
+		$image_id = (int) get_field('home__img');
+		// Création d'une taille d'image avec la fonction add_image_size();
+		$taille_image = 'test';
+		// Fonction WordPress qui stocke la balise img complète
+		// https://developer.wordpress.org/reference/functions/wp_get_attachment_image/
+		$img = wp_get_attachment_image($image_id, $taille_image); 
+		?>
+		
+		<?php 	
+			// je vérfie si mon champ existe
+			if ( get_field('home__img') ) { 
+		?>
+            <div class="home__img">
+            <?php 
+				// Affichage la balise img avec echo
+				echo $img; 
+			?>
+            </div>
+		
+		<?php } ?>
                 </div>
             </div>
         </section>
 
+        <?php if (have_rows('definition')): while(have_rows('definition')): the_row() ?>
         <section class="section">
             <div class="container">
                 <div class="definition__box">
-                    <h1 class="definition__title">Qu’est-ce que c’est la sobriété numérique ?</h1>
-                    <p class="definition__description">La sobriété numérique est un concept qui vise à réduire l’empreinte carbone du secteur du numérique, notamment en matière de consommation énergétique.</p>
+                    <h1 class="definition__title"><?php the_sub_field('title') ?></h1>
+                    <p class="definition__description"><?php the_sub_field('description') ?></p>
                 </div>
             </div>
         </section>
+        <?php endwhile; endif ?>
 
-        <!---
+        <!--
         <section class="section">
                 <div class="container">
                     <h2 class="section__title">Quelques chiffres</h2>
-                    <div class="number__container grid">
-                        <div class="number__card">
-                            <h2 class="number__title">35%</h2>
-                            <div class="number__divider"></div>
-                            <p class="number__description">C’est la réduction de l’impact carbone lorsque vous faites une recherche par URL (si vous la connaissez !) au lieu de faire une recherche Google.</p>
-                        </div>
-                        <div class="number__card">
-                            <h2 class="number__title">35%</h2>
-                            <div class="number__divider"></div>
-                            <p class="number__description">C’est la réduction de l’impact carbone lorsque vous faites une recherche par URL (si vous la connaissez !) au lieu de faire une recherche Google.</p>
-                        </div>
-                        <div class="number__card">
-                            <h2 class="number__title">35%</h2>
-                            <div class="number__divider"></div>
-                            <p class="number__description">C’est la réduction de l’impact carbone lorsque vous faites une recherche par URL (si vous la connaissez !) au lieu de faire une recherche Google.</p>
-                        </div>
-                    </div>
+                        <ul class="carousel-items">
+                            <li class="carousel-item">
+                                <div class="number__card">
+                                    <h2 class="number__title">35%</h2>
+                                    <div class="number__divider"></div>
+                                    <p class="number__description">C’est la réduction de l’impact carbone lorsque vous faites une recherche par URL (si vous la connaissez !) au lieu de faire une recherche Google.</p>
+                                </div>
+                            </li>
+                            <li class="carousel-item">
+                                <div class="number__card">
+                                    <h2 class="number__title">35%</h2>
+                                    <div class="number__divider"></div>
+                                    <p class="number__description">C’est la réduction de l’impact carbone lorsque vous faites une recherche par URL (si vous la connaissez !) au lieu de faire une recherche Google.</p>
+                                </div>
+                            </li>
+                            <li class="carousel-item">
+                                <div class="number__card">
+                                    <h2 class="number__title">35%</h2>
+                                    <div class="number__divider"></div>
+                                    <p class="number__description">C’est la réduction de l’impact carbone lorsque vous faites une recherche par URL (si vous la connaissez !) au lieu de faire une recherche Google.</p>
+                                </div>
+                            </li>
+                        </ul>
                 </div>
             </div>
         </section>
-        --->
+-->
 
         <section class="section">
             <div class="container">
@@ -92,44 +120,16 @@
             <div class="container">
                 <div class="news__container">
                     <h2 class="section__title">L'actualité des pros du digital</h2>
-                    <a href="#">TOUT VOIR</a>
+                    <a href="/blog/">TOUT VOIR</a>
                 </div>
-                <div class="news__searchbar">
-                    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M22 22L16.6845 16.6845M16.6845 16.6845C17.5377 15.8313 18.2144 14.8185 18.6762 13.7038C19.1379 12.5891 19.3755 11.3943 19.3755 10.1878C19.3755 8.98121 19.1379 7.78647 18.6762 6.67176C18.2144 5.55705 17.5377 4.5442 16.6845 3.69103C15.8313 2.83787 14.8185 2.16111 13.7038 1.69938C12.5891 1.23765 11.3943 1 10.1878 1C8.98121 1 7.78647 1.23765 6.67176 1.69938C5.55705 2.16111 4.5442 2.83787 3.69103 3.69103C1.96799 5.41407 1 7.75102 1 10.1878C1 12.6245 1.96799 14.9615 3.69103 16.6845C5.41407 18.4075 7.75102 19.3755 10.1878 19.3755C12.6245 19.3755 14.9615 18.4075 16.6845 16.6845V16.6845Z" stroke="#F3F3E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>                        
-                    <input type="text" placeholder="Rechercher des articles par thème">
-                </div>
-                    <div class="news__card">
-                        <img src="assets/img/news.jpg" alt="news" class="news__img">
-                        <div class="news__content">
-                            <div class="news__category">
-                                <p>Low-tech</p>
-                                <p>25 novembre 2022</p>
-                            </div>
-                            <a href="#" class="news__title">5 conseils de Paul Muller pour développer en dimuniant son impact Co2 en 2022</a>
-                        </div>
-                    </div>
-                    <div class="news__card">
-                        <img src="assets/img/news.jpg" alt="news" class="news__img">
-                        <div class="news__content">
-                            <div class="news__category">
-                                <p>Low-tech</p>
-                                <p>25 novembre 2022</p>
-                            </div>
-                            <a href="#" class="news__title">5 conseils de Paul Muller pour développer en dimuniant son impact Co2 en 2022</a>
-                        </div>
-                    </div>
-                    <div class="news__card">
-                        <img src="assets/img/news.jpg" alt="news" class="news__img">
-                        <div class="news__content">
-                            <div class="news__category">
-                                <p>Low-tech</p>
-                                <p>25 novembre 2022</p>
-                            </div>
-                            <a href="#" class="news__title">5 conseils de Paul Muller pour développer en dimuniant son impact Co2 en 2022</a>
-                        </div>
-                    </div>
+                <?php get_search_form (); ?>
+                <?php
+                    $recentPosts = new WP_Query();
+                    $recentPosts->query('showposts=5');
+                ?>
+                <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+                    <?php get_template_part('template-parts/post'); ?>
+                <?php endwhile; ?>
         </section>
 
         <section class="section">
@@ -137,14 +137,7 @@
                 <div class="newsletter__box">
                     <h1 class="newsletter__title">USEGREEN NEWSLETTER</h1>
                     <p class="newsletter__description">Être informé de nos dernières actualités pour un meilleur réflexe éco-responsable dans vos conceptions et manière d’utilisation du numérique.</p>
-                    <div class="newsletter__searchbar">
-                        <input type="text" placeholder="Votre adresse mail ici">
-                        <button class="newsletter__button">
-                            <svg width="20" height="8" viewBox="0 0 20 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M19.3536 4.35356C19.5488 4.15829 19.5488 3.84171 19.3536 3.64645L16.1716 0.464468C15.9763 0.269205 15.6597 0.269205 15.4645 0.464467C15.2692 0.65973 15.2692 0.976312 15.4645 1.17157L18.2929 4L15.4645 6.82843C15.2692 7.02369 15.2692 7.34027 15.4645 7.53554C15.6597 7.7308 15.9763 7.7308 16.1716 7.53554L19.3536 4.35356ZM-4.37114e-08 4.5L19 4.5L19 3.5L4.37114e-08 3.5L-4.37114e-08 4.5Z" fill="#1D1D1B"/>
-                            </svg>  
-                        </button>
-                    </div>
+                    <?= do_shortcode('[wpforms id="128" html_class="newsletter__searchbar"]'); ?>
                 </div>
             </div>
         </section>
--- wp-content/themes/usegreen/functions.php
+++ wp-content/themes/usegreen/functions.php
@@ -29,19 +29,32 @@ add_theme_support( 'post-thumbnails' );
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-
 // Ajouter le logo
 add_theme_support('custom-logo');
 
-// Prise en charge SVG
-function wpc_mime_types($mimes) {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+// Hooks
+add_filter( 'upload_mimes', 'capitaine_mime_types' );
+add_filter( 'wp_check_filetype_and_ext', 'capitaine_file_types', 10, 4 );
+
+// Autoriser l'import des fichiers SVG et WEBP
+function capitaine_mime_types( $mimes ){
+    $mimes['svg'] = 'image/svg+xml';
+    $mimes['webp'] = 'image/webp';
+    return $mimes;
 }
-add_filter('upload_mimes', 'wpc_mime_types');
+
+// Contrôle de l'import d'un WEBP
+function capitaine_file_types( $types, $file, $filename, $mimes ) {
+	if ( false !== strpos( $filename, '.webp' ) ) {
+    	$types['ext'] = 'webp';
+   		$types['type'] = 'image/webp';
+	}
+	return $types;
+}
 
 function header_function() {
     register_nav_menu('header',__( 'Main navigation' ));
 }
 add_action( 'init', 'header_function' );
+
+add_image_size( 'test', 650, 650, true);
