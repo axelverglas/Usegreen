2f4df160788bc018d6bf582cdfefbc59eab1f1e2
Axel Verglas <axelverglas@outlook.com>
2022-12-07 09:54:56 +0100

    Modification menu toggle

--- wp-content/themes/usegreen/assets/js/menu.js
+++ wp-content/themes/usegreen/assets/js/menu.js
@@ -2,10 +2,8 @@
 
 let toggle = document.querySelector(".nav__toggle");
 let body = document.querySelector("body");
-let nav__menu = document.querySelector(".nav__menu");
 
 toggle.addEventListener("click", function () {
   body.classList.toggle("open");
-  nav__menu.classList.add("open");
-  body.style.overflowY = "hidden";
+  body.classList.toggle("overflow");
 });
